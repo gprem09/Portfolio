774eb2d48432a67bec4df1e2258495bd75dd89b6
gpa21 <gpa21@sfu.ca>
2023-11-05 16:25:04 -0800

    updating

--- src/app/page.js
+++ src/app/page.js
@@ -99,9 +99,15 @@ export default function Home() {
       <section className="min-h-screen">
         <nav className='py-10 mb-12 flex justify-between'>
         <div className={`flex items-center gap-8 transition ${!isDarkMode ? 'bg-color-text' : 'text-custom-cyan'}`}>
-          <AiFillLinkedin className='text-2xl'/>
-          <AiFillMail className='text-2xl'/>
-          <AiFillGithub className='text-2xl'/>
+          <a href="https://www.linkedin.com/in/gnanavel-premnath-b27a53235/" target="_blank" rel="noopener noreferrer">
+            <AiFillLinkedin className='text-2xl'/>
+          </a>
+          <a href="mailto:gpa21@sfu.ca" target="_blank" rel="noopener noreferrer">
+            <AiFillMail className='text-2xl'/>
+          </a>
+          <a href="https://github.com/gprem09" target="_blank" rel="noopener noreferrer">
+            <AiFillGithub className='text-2xl'/>
+          </a>
         </div>
           
           <ul className='flex item-center'>
